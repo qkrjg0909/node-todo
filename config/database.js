02728e1863ef6278a0ddccaf56096143b33da59e
Brody Park <test@naver.com>
2018-07-27 17:20:45 +0900

    modify commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://nodetodoapp1:029ppaI9gdY3hyQhCBan5vJzDkSJN5z4LOFj31isUPUaO0JyIDYzukKBMXanr6XjTnNpgXjcgPup29f3q9K8uw==@nodetodoapp1.documents.azure.com:10255/?ssl=true&replicaSet=globaldb',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
